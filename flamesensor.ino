3fcc826c27d4e34b3bf8689c1eefcdfce5c46525
jose marcelino <josemarcelino@josemarcelino-laptop.(none)>
2015-02-28 04:23:36 +0000

    rektDatCandle

--- flamesensor.ino
+++ flamesensor.ino
@@ -2,13 +2,41 @@
 // pinMode(A1, INPUT);
 // int nearLimit = 30;
 
-/*boolean canISeeTheFlame() {
+boolean canISeeTheFlame() {
 
 	int valueFlameSensor = analogRead(A1);
 
 	if(valueFlameSensor >= nearLimit)
-		return 1;
+		return 1; // 
 	else
 		return 0;
 
-}*/
+}
+
+void lookForFireDirection(){
+//Robot already inside Room, fire confirmed
+	int max = analogRead(A1);
+	int actualread = max;
+
+	while(max <= actualRead){
+		max = actualRead;
+		actualRead = analogRead(A1);
+		// rotate()
+	}
+
+	//goForward()
+	//rektDatCandle()
+}
+
+
+
+
+
+
+	
+
+
+
+
+
+
